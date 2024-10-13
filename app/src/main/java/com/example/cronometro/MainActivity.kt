a33aa2e93c7d2268a4bd6108bfa02de58023995f
Nobru <nobrudeveloper@gmail.com>
2024-10-13 14:27:46 -0300

    Teste de funcionalidade.

--- app/src/main/java/com/example/cronometro/MainActivity.kt
+++ app/src/main/java/com/example/cronometro/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.cronometro
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+        //val editText: EditText = findViewById(R.id.edit_value)
+        val buttonStart: Button = findViewById(R.id.btn_start)
+        //val buttonStop: Button = findViewById(R.id.btn_stop)
+        //val result: TextView = findViewById(R.id.txt_result)
+
+        buttonStart.setOnClickListener{
+            Toast.makeText(this, "Teste", Toast.LENGTH_SHORT).show()
         }
     }
 }
